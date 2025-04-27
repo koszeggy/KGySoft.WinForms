ac413e474ca13d1651c0cdc81823710f02f90c23
György Kőszeg <koszeggy@gmail.com>
2025-04-27 20:42:18 +0200

    Fixing disabled image generation

---
+++ KGySoft.WinForms/_Extensions/ImageExtensions.cs
@@ -0,0 +1,38 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: ImageExtensions.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2025 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using KGySoft.Drawing;
+using KGySoft.Drawing.Imaging;
+
+#endregion
+
+namespace KGySoft.WinForms
+{
+    internal static class ImageExtensions
+    {
+        #region Methods
+
+        // TODO: Remove when KGySoft.Drawing.ImageExtensions.ToGrayscale is fixed (does not return solid background anymore)
+        internal static Image ToGrayscale(this Image image)
+            => image.ConvertPixelFormat(PixelFormat.Format32bppArgb, PredefinedColorsQuantizer.FromCustomFunction(c => c.ToGray(), default, 0, autoBlend: false));
+
+        #endregion
+    }
+}
