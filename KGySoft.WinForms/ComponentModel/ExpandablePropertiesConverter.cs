b342384a6db898ff5182e89cc8d0c476651bb3b5
György Kőszeg <koszeggy@gmail.com>
2025-05-20 13:37:52 +0200

    Extracting ExpandablePropertiesConverter from RecursivelyEditableTypeDescriptor and making it public

---
+++ KGySoft.WinForms/ComponentModel/ExpandablePropertiesConverter.cs
@@ -0,0 +1,126 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: ExpandablePropertiesConverter.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2025 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System;
+using System.ComponentModel;
+
+#endregion
+
+// ReSharper disable once CheckNamespace
+namespace KGySoft.ComponentModel
+{
+    /// <summary>
+    /// Similar to <see cref="ExpandableObjectConverter"/> but recursively.
+    /// </summary>
+    public class ExpandablePropertiesConverter : TypeConverter
+    {
+        #region Nested classes
+
+        #region ExpandablePropertyDescriptor class
+
+        private class ExpandablePropertyDescriptor : PropertyDescriptor
+        {
+            #region Fields
+
+            private readonly PropertyDescriptor wrappedDescriptor;
+
+            private TypeConverter? converter;
+
+            #endregion
+
+            #region Properties
+
+            public override Type ComponentType => wrappedDescriptor.ComponentType;
+            public override bool IsReadOnly => wrappedDescriptor.IsReadOnly;
+            public override Type PropertyType => wrappedDescriptor.PropertyType;
+            public override TypeConverter Converter => converter ??= new ExpandablePropertiesConverter(wrappedDescriptor.PropertyType);
+
+            #endregion
+
+            #region Constructors
+
+            internal ExpandablePropertyDescriptor(PropertyDescriptor descriptor) : base(descriptor)
+            {
+                wrappedDescriptor = descriptor;
+            }
+
+            #endregion
+
+            #region Methods
+
+            public override bool CanResetValue(object component) => wrappedDescriptor.CanResetValue(component);
+            public override object? GetValue(object? component) => wrappedDescriptor.GetValue(component);
+            public override void ResetValue(object component) => wrappedDescriptor.ResetValue(component);
+            public override void SetValue(object? component, object? value) => wrappedDescriptor.SetValue(component, value);
+            public override bool ShouldSerializeValue(object component) => wrappedDescriptor.ShouldSerializeValue(component);
+
+            #endregion
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Fields
+
+        private readonly Type type;
+
+        #endregion
+
+        #region Constructors
+
+        public ExpandablePropertiesConverter(Type type) => this.type = type ?? throw new ArgumentNullException(nameof(type));
+
+        #endregion
+
+        #region Methods
+
+        #region Static Methods
+
+        internal static PropertyDescriptorCollection FilterProperties(PropertyDescriptorCollection properties, Type type)
+        {
+            var result = new PropertyDescriptorCollection(null);
+
+            foreach (PropertyDescriptor property in properties)
+            {
+                if (!(property.Converter is ReferenceConverter || property.Converter.GetType() == typeof(TypeConverter)))
+                {
+                    result.Add(property);
+                    continue;
+                }
+
+                result.Add(new ExpandablePropertyDescriptor(property));
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext? context, object value, Attribute[]? attributes)
+            => FilterProperties(TypeDescriptor.GetProperties(value, attributes, true), type);
+
+        public override bool GetPropertiesSupported(ITypeDescriptorContext? context)
+            => context?.PropertyDescriptor?.PropertyType.GetProperties().Length > 0;
+
+        #endregion
+
+        #endregion
+    }
+}
--- KGySoft.WinForms/ComponentModel/RecursivelyEditableTypeDescriptor.cs
+++ KGySoft.WinForms/ComponentModel/RecursivelyEditableTypeDescriptor.cs
@@ -28,75 +28,6 @@ namespace KGySoft.ComponentModel
     /// </summary>
     public class RecursivelyEditableTypeDescriptor : ICustomTypeDescriptor
     {
-        #region Nested classes
-
-        #region ExpandablePropertyDescriptor class
-
-        class ExpandablePropertyDescriptor : PropertyDescriptor
-        {
-            #region Fields
-
-            private readonly PropertyDescriptor wrappedDescriptor;
-            private readonly object obj;
-            private TypeConverter converter;
-
-            #endregion
-
-            #region Properties
-
-            public override Type ComponentType => wrappedDescriptor.ComponentType;
-            public override bool IsReadOnly => wrappedDescriptor.IsReadOnly;
-            public override Type PropertyType => obj?.GetType() ?? wrappedDescriptor.PropertyType;
-            public override TypeConverter Converter => obj == null ? wrappedDescriptor.Converter : converter ?? (converter = new ExpandablePropertiesConverter(obj));
-
-            #endregion
-
-            #region Constructors
-
-            public ExpandablePropertyDescriptor(PropertyDescriptor descriptor, object obj) : base(descriptor)
-            {
-                wrappedDescriptor = descriptor;
-                this.obj = obj;
-            }
-
-            #endregion
-
-            #region Methods
-
-            public override bool CanResetValue(object component) => wrappedDescriptor.CanResetValue(component);
-            public override object GetValue(object component) => wrappedDescriptor.GetValue(component);
-            public override void ResetValue(object component) => wrappedDescriptor.ResetValue(component);
-            public override void SetValue(object component, object value) => wrappedDescriptor.SetValue(component, value);
-            public override bool ShouldSerializeValue(object component) => wrappedDescriptor.ShouldSerializeValue(component);
-
-            #endregion
-        }
-
-        #endregion
-
-        #region ExpandablePropertiesConverter class
-
-        internal class ExpandablePropertiesConverter : TypeConverter
-        {
-            private readonly object obj;
-
-            internal ExpandablePropertiesConverter(object obj) => this.obj = obj;
-
-            #region Methods
-
-            public override bool GetPropertiesSupported(ITypeDescriptorContext context)
-                => context.PropertyDescriptor?.PropertyType.GetProperties().Length > 0;
-
-            public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes) 
-                => FilterProperties(TypeDescriptor.GetProperties(value, attributes, true), obj);
-
-            #endregion
-        }
-
-        #endregion
-
-        #endregion
-
         #region Properties
 
         public object Object { get; }
@@ -114,45 +45,18 @@ namespace KGySoft.ComponentModel
 
         #region Methods
 
-        #region Public Methods
-
         public AttributeCollection GetAttributes() => TypeDescriptor.GetAttributes(Object, true);
-        public string GetClassName() => TypeDescriptor.GetClassName(Object, true);
-        public string GetComponentName() => TypeDescriptor.GetComponentName(Object, true);
+        public string? GetClassName() => TypeDescriptor.GetClassName(Object, true);
+        public string? GetComponentName() => TypeDescriptor.GetComponentName(Object, true);
         public TypeConverter GetConverter() => TypeDescriptor.GetConverter(Object, true);
-        public EventDescriptor GetDefaultEvent() => TypeDescriptor.GetDefaultEvent(Object, true);
-        public PropertyDescriptor GetDefaultProperty() => TypeDescriptor.GetDefaultProperty(Object, true);
-        public object GetEditor(Type editorBaseType) => TypeDescriptor.GetEditor(Object, editorBaseType, true);
+        public EventDescriptor? GetDefaultEvent() => TypeDescriptor.GetDefaultEvent(Object, true);
+        public PropertyDescriptor? GetDefaultProperty() => TypeDescriptor.GetDefaultProperty(Object, true);
+        public object? GetEditor(Type editorBaseType) => TypeDescriptor.GetEditor(Object, editorBaseType, true);
         public EventDescriptorCollection GetEvents() => TypeDescriptor.GetEvents(Object, true);
-        public EventDescriptorCollection GetEvents(Attribute[] attributes) => TypeDescriptor.GetEvents(Object, attributes, true);
-        public PropertyDescriptorCollection GetProperties() => FilterProperties(TypeDescriptor.GetProperties(Object, true), Object);
-        public PropertyDescriptorCollection GetProperties(Attribute[] attributes) => FilterProperties(TypeDescriptor.GetProperties(Object, attributes, true), Object);
-        public object GetPropertyOwner(PropertyDescriptor pd) => Object;
-
-        #endregion
-
-        #region Private Methods
-
-        private static PropertyDescriptorCollection FilterProperties(PropertyDescriptorCollection properties, object instance)
-        {
-            var result = new PropertyDescriptorCollection(null);
-
-            foreach (PropertyDescriptor property in properties)
-            {
-                if (!(property.Converter is ReferenceConverter || property.Converter?.GetType() == typeof(TypeConverter)))
-                {
-                    result.Add(property);
-                    continue;
-                }
-
-                object propertyValue = property.GetValue(instance);
-                result.Add(new ExpandablePropertyDescriptor(property, propertyValue));
-            }
-
-            return result;
-        }
-
-        #endregion
+        public EventDescriptorCollection GetEvents(Attribute[]? attributes) => TypeDescriptor.GetEvents(Object, attributes, true);
+        public PropertyDescriptorCollection GetProperties() => ExpandablePropertiesConverter.FilterProperties(TypeDescriptor.GetProperties(Object, true), Object.GetType());
+        public PropertyDescriptorCollection GetProperties(Attribute[]? attributes) => ExpandablePropertiesConverter.FilterProperties(TypeDescriptor.GetProperties(Object, attributes, true), Object.GetType());
+        public object GetPropertyOwner(PropertyDescriptor? pd) => Object;
 
         #endregion
     }
