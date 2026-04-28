1db0b7faa0db4fa11b247028bd6eb6ba2a9e0725
György Kőszeg <koszeggy@gmail.com>
2026-04-28 11:37:36 +0200

    Releasing version v5.0.0-rc.3

--- KGySoft.WinForms/Properties/AssemblyInfo.cs
+++ KGySoft.WinForms/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Security;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("5.0.0.0")]
 [assembly: AssemblyFileVersion("5.0.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-rc.2")]
+[assembly: AssemblyInformationalVersion("5.0.0-rc.3")]
 #if NET5_0_OR_GREATER
 [assembly: SupportedOSPlatform("windows")] 
 #endif
